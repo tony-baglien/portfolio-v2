52fbc975a930b911d270fd0a48b35078fdbfd728
Toni <baglient@gmail.com>
2024-06-05 22:30:51 -0500

    fixed mobile name tab bug

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,16 +1,15 @@
-
-import React, { useRef } from 'react'
-import { useGSAP } from '@gsap/react'
-import TiltWrapper from "./Util/TiltWrapper"
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import  { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import TiltWrapper from "./Util/TiltWrapper";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export const Header = () => {
   const navRef = useRef();
   const navContentsRef = useRef();
 
   useGSAP(() => {
-    gsap.registerPlugin(ScrollTrigger)
+    gsap.registerPlugin(ScrollTrigger);
 
     let anime = gsap.to(navRef.current, {
       yPercent: -200,
@@ -23,20 +22,26 @@ export const Header = () => {
         end: "max",
         onUpdate: (self) => {
           if (self.direction === 1) {
-            anime.play()
+            anime.play();
           } else {
-            anime.reverse()
+            anime.reverse();
           }
-        }
-      }
-    })
-  })
+        },
+      },
+    });
+  });
   return (
     <TiltWrapper xTilt={2} yTilt={2}>
-      <nav ref={navRef} className='w-full h-8 flex sticky top-6 z-50 items-center justify-center my-6 '>
+      <nav
+        ref={navRef}
+        className="hidden md:flex w-full h-8 sticky top-6 z-50 items-center justify-center my-6 "
+      >
         <ul className=" glass flex flex-row h-auto w-4/6 items-center justify-center rounded-xl rounded-tl-none rounded-tr-none ml-10 py-0 px-8">
           <li>
-            <span ref={navContentsRef} className="
+            <span
+              ref={navContentsRef}
+              className="
+              
         block
         w-full
         text-h3
@@ -44,10 +49,13 @@ export const Header = () => {
         font-rubix
         text-accent
         text-center
-        rounded-4xl">Toni Baglien</span>
-          </li >
-        </ul >
-      </nav >
+        rounded-4xl"
+            >
+              Toni Baglien
+            </span>
+          </li>
+        </ul>
+      </nav>
     </TiltWrapper>
-  )
-}
+  );
+};
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8000,
+    cors: true,
+    hmr: true,
+  },
+  watch: {
+    usePolling: true,
+  }
 })
